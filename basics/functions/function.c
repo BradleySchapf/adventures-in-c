30cb0a938a1129480eb86b5caeead263cfb49ed1
Bradley Schapf <bradleyschapf@icloud.com>
2025-06-23 21:40:31 +0200

    function example

---
+++ basics/functions/function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void happyBirthday() {
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday dear ... YOU!");
+    printf("\nHappy birthday to you!\n");
+}
+
+int main () {
+    happyBirthday();
+    happyBirthday();
+    happyBirthday();
+
+    return 0;
+}
