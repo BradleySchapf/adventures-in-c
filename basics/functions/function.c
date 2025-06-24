9f2f6774b53e35f4aae2b8608a90704d1cfe2a72
Bradley Schapf <bradleyschapf@icloud.com>
2025-06-24 20:25:08 +0200

    parameter example

--- basics/functions/function.c
+++ basics/functions/function.c
@@ -7,10 +7,23 @@ void happyBirthday() {
     printf("\nHappy birthday to you!\n");
 }
 
+void happyBirthdayGreeting(int age, char name[]) {
+    printf("\nHappy birthday dear %s!", name);
+    printf("\nyou are %d", age);
+
+}
+
+// returns a double
+double squared(int num) {
+    return num * num;
+}
+
 int main () {
     happyBirthday();
     happyBirthday();
     happyBirthday();
+
+    happyBirthdayGreeting(25, "Brad");
 
     return 0;
 }
